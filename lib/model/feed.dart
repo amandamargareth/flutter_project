b61fc238ea026a0a539b4727bea826e22f403df3
Amanda Margaretha <amandamargaretha272011@gmail.com>
2024-11-06 19:58:55 +0700

    [TUGAS-SESI-5] feature(<model>): <<icon like>>

--- lib/model/feed.dart
+++ lib/model/feed.dart
@@ -4,33 +4,30 @@ class Feed {
   //Content
   final Content content;
 
-  Feed({
-    required this.user, 
-    required this.content
-  });
+  Feed({required this.user, required this.content});
 }
 
 class Content {
   final String image;
+  bool favorite;
   final String likes;
   final String descriptions;
 
-  Content({
-    required this.image, 
-    required this.likes, 
-    required this.descriptions
-  });
+  Content(
+      {required this.image,
+      this.favorite = false,
+      required this.likes,
+      required this.descriptions});
+
+  void toggleFavorite() {
+    favorite = !favorite;
+  }
 }
 
 class User {
-final String name;
-final String avatar;
-final String place;
+  final String name;
+  final String avatar;
+  final String place;
 
-  User({
-    required this.name, 
-    required this.avatar, 
-    required this.place
-  });
-
+  User({required this.name, required this.avatar, required this.place});
 }
--- lib/view/feed_cart.dart
+++ lib/view/feed_cart.dart
@@ -3,64 +3,63 @@ import 'package:flutter_sesi4/model/feed.dart';
 
 class FeedCard extends StatelessWidget {
   final Feed feed;
-  
-  const FeedCard({
-    super.key,
-    required this.feed
-  });
+
+  const FeedCard({super.key, required this.feed});
 
   @override
   Widget build(BuildContext context) {
-    const url = 'https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg';
     return Card(
-      child: Column(
-        children: [
-          ListTile(
-            leading: CircleAvatar(
-              backgroundImage: NetworkImage(
-                feed.user.avatar
-              ),
-            ),
-            title: Text(feed.user.name),
-            subtitle: Text(feed.user.place),
-            trailing: Icon(Icons.share),
+      child: Column(children: [
+        ListTile(
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(feed.user.avatar),
           ),
-          Image.network(feed.content.image,
+          title: Text(feed.user.name),
+          subtitle: Text(feed.user.place),
+          trailing: Icon(Icons.more_vert_rounded),
+        ),
+        Image.network(
+          feed.content.image,
           width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.width * 0.8,
           fit: BoxFit.cover,
-          ),
-          Row(
-            children: <Widget>[
-    SizedBox(width:10),
-    Icon(
-      Icons.favorite,
-      color: Colors.red,
-      size: 24.0,
-      semanticLabel: 'Text to announce in accessibility modes',
-    ),
-    SizedBox(width:10),
-    Icon(
-      Icons.chat_bubble_outline_sharp,
-      size: 24.0,
-    ),
-    SizedBox(width:10),
-    Icon(
-      Icons.share,
-      size: 24.0,
-    ),
-      Spacer(flex: 1,),
-              Padding(
-                padding: const EdgeInsets.only(right: 8.0),
-                child: Icon(Icons.bookmark_outline),
-              )
-  ],
-          ),
-ListTile(
-  title: Text(feed.content.likes),
-  subtitle: Text(feed.content.descriptions),
-)
-
+        ),
+        SizedBox(height: 10),
+        Row(
+          children: <Widget>[
+            SizedBox(width: 10),
+            Icon(
+              feed.content.favorite
+                  ? Icons.favorite
+                  : Icons.favorite_outline_rounded,
+              color: feed.content.favorite
+                  ? Colors.red
+                  : null, // Tidak ada warna jika false
+              size: 24.0,
+            ),
+            SizedBox(width: 10),
+            Icon(
+              Icons.mode_comment_outlined,
+              size: 24.0,
+            ),
+            SizedBox(width: 10),
+            Icon(
+              Icons.send_outlined,
+              size: 24.0,
+            ),
+            Spacer(
+              flex: 1,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: Icon(Icons.bookmark_outline_rounded),
+            )
+          ],
+        ),
+        ListTile(
+          title: Text(feed.content.likes),
+          subtitle: Text(feed.content.descriptions),
+        )
       ]),
     );
   }
